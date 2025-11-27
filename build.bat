0f8a228d1ecd32a0903174f711406e4eb4148603
cezar17x <checharinmarquez@gmail.com>
2025-11-27 01:09:02 -0300

    pequeña correccion

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Construyendo ejecutable...
-python -m PyInstaller --onefile --clean src\\calculadora.py
+python -m PyInstaller --onefile --console --clean calculadora.py
 echo EXE generado en carpeta dist
 pause
