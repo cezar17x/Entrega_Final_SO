489ec2361bda483503b58b19b709d2f125da859d
cezar17x <checharinmarquez@gmail.com>
2025-11-27 00:47:54 -0300

    Listo

---
+++ ADR.md
@@ -0,0 +1,6 @@
+# ADR - Calculadora CLI
+Decisiones técnicas:
+- Python seleccionado por portabilidad.
+- PyInstaller para conversión a EXE.
+- Inno Setup recomendado para instalador.
+- CLI por exigencias de Sistemas Operativos.
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,8 +1,6 @@
 import subprocess
 
-
 def test_suma():
-    result = subprocess.run(["dist/calculadora.exe", "sumar", "2", "3"], capture_output=True, text=True)
+    result = subprocess.run(["dist/calculadora.exe", "sumar", "2", "3"],
+    capture_output=True, text=True)
     assert "5" in result.stdout
-
-test_suma()
